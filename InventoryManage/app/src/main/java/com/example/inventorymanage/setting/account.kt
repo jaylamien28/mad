bcdad9e4c7d1713be60870e630c9b3bb596edc0b
jaylamien28 <hengmien060728@gmail.com>
2025-09-06 16:33:10 +0800

    Update account.kt

---
+++ InventoryManage/app/src/main/java/com/example/inventorymanage/setting/account.kt
@@ -0,0 +1,20 @@
+package com.example.inventorymanage.setting
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun login() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+    )
+}
+
